cac586315dc61c34dfac98fa1af70feb33b11566
221110019 <221110019@students.mikroskil.ac.id>
2025-04-30 03:21:22 +0700

    basic CRUD Tasks succesfully

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\NotesController;
+use App\Http\Controllers\TasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/notes', NotesController::class);
+Route::apiResource('/tasks', TasksController::class);
