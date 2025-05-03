305564d6001a70332d7835a35f646b9abc80f22f
221110019 <221110019@students.mikroskil.ac.id>
2025-05-03 18:16:16 +0700

    after deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,16 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        if (App::environment('production')) {
+            config([
+                'logging.channels.stack' => [
+                    'driver' => 'single',
+                    'path' => '/tmp/laravel.log',
+                    'level' => 'debug',
+                ],
+                'view.compiled' => '/tmp',
+                'cache.stores.file.path' => '/tmp',
+            ]);
+        }
     }
 }
